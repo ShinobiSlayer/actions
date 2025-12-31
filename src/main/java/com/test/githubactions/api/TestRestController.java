800ef8e20410f91fe11cf5fe693d024ec4b1ab44
davidharland <david.harland2@gmail.com>
2025-12-31 15:26:41 +0000

    Test two-repo CI/CD flow

--- src/main/java/com/test/githubactions/api/TestRestController.java
+++ src/main/java/com/test/githubactions/api/TestRestController.java
@@ -10,6 +10,6 @@ public class TestRestController {
 
     @GetMapping("/test")
     public Map<String, String> get() {
-        return Map.of("message", "Hello World2!");
+        return Map.of("message", "Hello World3!");
     }
 }
