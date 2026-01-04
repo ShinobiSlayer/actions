927cbff2a2ce95bfc005721b2c54a033e89e3c18
davidharland <david.harland2@gmail.com>
2026-01-04 10:59:00 +0000

    Testing e2e 12

--- src/main/java/com/test/githubactions/api/TestRestController.java
+++ src/main/java/com/test/githubactions/api/TestRestController.java
@@ -10,6 +10,6 @@ public class TestRestController {
 
     @GetMapping("/test")
     public Map<String, String> get() {
-        return Map.of("message", "Hello World11!");
+        return Map.of("message", "Hello World12!");
     }
 }
